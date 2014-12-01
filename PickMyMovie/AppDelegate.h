2ed1e6856eaa2f6c49210a2ca45a040219b4387f
Niklas Persson <niklaspersson1988@gmail.com>
2014-12-01 20:37:01 +0100

    Add coredata

---
+++ PickMyMovie/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  PickMyMovie
+//
+//  Created by Niklas Persson on 11/12/14.
+//  Copyright (c) 2014 NPFLASH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+
+@end
